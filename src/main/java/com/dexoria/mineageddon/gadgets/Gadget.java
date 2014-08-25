785a85c6ff19734727791f44750c6003282cb525
nxsupert <ethanmwebb@gmail.com>
2014-08-25 16:32:54 +0100

    Started event system.

--- src/main/java/com/dexoria/mineageddon/gadgets/Gadget.java
+++ src/main/java/com/dexoria/mineageddon/gadgets/Gadget.java
@@ -4,7 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import com.dexoria.mineageddon.Mineageddon;
 import com.dexoria.mineageddon.references.Debug;
@@ -13,8 +16,11 @@ public class Gadget {
 	// Static stuff. 
 	private static Map<String,String> itemToGadgetIdList;
 	private static Map<String,Gadget> gadgets;
-	
+	private static GadgetEventListener listener;
 	public static void onEnable() {
+		listener = new GadgetEventListener();
+		Bukkit.getServer().getPluginManager().registerEvents(listener, Mineageddon.getInstance());
+		
 		itemToGadgetIdList = new HashMap<String,String>();
 		gadgets = new HashMap<String,Gadget>();
 		
@@ -22,8 +28,21 @@ public class Gadget {
 	}
 	
 	public static void onDisable() {
+		
 		itemToGadgetIdList.clear();
 		gadgets.clear();
+		
+		HandlerList.unregisterAll(listener);
+		listener = null;
+	}
+	
+
+	public static boolean isVanillaItemGadget(String vanillaID) {
+		return itemToGadgetIdList.containsKey(vanillaID);
+	}
+	
+	public static Gadget getGadgetFromVanillaID(String vanillaID) {
+		return gadgets.get(itemToGadgetIdList.get(vanillaID));
 	}
 	
 	private final String name;
@@ -40,5 +59,13 @@ public class Gadget {
 		gadgets.put(name, this);
 	}
 	
+	public boolean onPlayerInteractEvent(PlayerInteractEvent event) {
+		if(Debug.ON) {
+			Mineageddon.getLoggerStaticly().log(Level.INFO,"onPlayerInteractEvent invoked for '" + name + "' by '" + event.getPlayer().getName() + "'.");
+		}
+		
+		return true;
+		
+	}
 	
 }
---
+++ src/main/java/com/dexoria/mineageddon/gadgets/GadgetEventListener.java
@@ -0,0 +1,16 @@
+package com.dexoria.mineageddon.gadgets;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class GadgetEventListener implements Listener{
+	 @EventHandler
+	 public boolean playerInteract(PlayerInteractEvent event) {
+		 String vinnilaID = event.getItem().getType().name() + ":" + event.getItem().getDurability();
+		 if(Gadget.isVanillaItemGadget(vinnilaID)) {
+			 return Gadget.getGadgetFromVanillaID(vinnilaID).onPlayerInteractEvent(event);
+		 }
+		 return true;
+	 }
+}
