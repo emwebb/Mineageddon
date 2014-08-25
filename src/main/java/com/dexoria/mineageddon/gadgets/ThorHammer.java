d53e87968519bece4fd4698deca3c8b1bf200dd8
nxsupert <ethanmwebb@gmail.com>
2014-08-25 19:55:54 +0100

    Added a way of create an explosion caused by a player!

--- src/main/java/com/dexoria/mineageddon/gadgets/ThorHammer.java
+++ src/main/java/com/dexoria/mineageddon/gadgets/ThorHammer.java
@@ -1,8 +1,15 @@
 package com.dexoria.mineageddon.gadgets;
 
+import net.minecraft.server.v1_7_R4.Entity;
+import net.minecraft.server.v1_7_R4.Explosion;
+import net.minecraft.server.v1_7_R4.World;
+
 import org.bukkit.Material;
+import org.bukkit.entity.Explosive;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import com.dexoria.mineageddon.utils.WorldUtils;
 
 public class ThorHammer extends Gadget {
 
@@ -13,7 +20,7 @@ public class ThorHammer extends Gadget {
 	@Override
 	public boolean onPlayerInteractEvent(PlayerInteractEvent event) {
 		if(event.getAction() == Action.LEFT_CLICK_BLOCK) {
-			event.getPlayer().getWorld().createExplosion(event.getClickedBlock().getLocation(), 2);
+			WorldUtils.createExplosion(event.getPlayer().getWorld(), event.getPlayer(), event.getClickedBlock().getLocation(), 5, false, true);
 		}
 		
 		return true;
---
+++ src/main/java/com/dexoria/mineageddon/utils/WorldUtils.java
@@ -0,0 +1,11 @@
+package com.dexoria.mineageddon.utils;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Entity;
+
+public class WorldUtils {
+	public static void createExplosion(World world,Entity entity, Location location, float power, boolean doFire, boolean doBlockDamage) {
+		((org.bukkit.craftbukkit.v1_7_R4.CraftWorld)world).getHandle().createExplosion(((org.bukkit.craftbukkit.v1_7_R4.entity.CraftEntity) entity).getHandle(), location.getX(),location.getY(),location.getZ(),power,doFire,doBlockDamage);
+	}
+}
