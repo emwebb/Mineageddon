a852e3d3624d1e44ad896f25a1a45d8926572dd8
nxsupert <ethanmwebb@gmail.com>
2014-10-03 22:25:47 +0100

    Added some Javadoc stuff.

--- src/main/java/com/dexoria/mineageddon/Mineageddon.java
+++ src/main/java/com/dexoria/mineageddon/Mineageddon.java
@@ -3,7 +3,6 @@ package com.dexoria.mineageddon;
 import java.sql.SQLException;
 import java.util.logging.Logger;
 
-import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.dexoria.mineageddon.command.CommandHandler;
@@ -12,43 +11,86 @@ import com.dexoria.mineageddon.gadgets.Gadget;
 import com.dexoria.mineageddon.mysql.MySQL;
 import com.dexoria.mineageddon.system.game.GameSystem;
 import com.dexoria.mineageddon.system.score.ScoreSystem;
-import com.dexoria.mineageddon.system.score.ScoreSystemListener;
 import com.dexoria.mineageddon.system.shop.ShopSystem;
 import com.dexoria.mineageddon.system.statistics.StatisticsSystem;
 
+/**
+ * @author nxsupert
+ *
+ */
 public class Mineageddon extends JavaPlugin {
+
+	/**
+	 * The static instance of {@link com.dexoria.mineageddon.Mineageddon} .
+	 */
 	private static Mineageddon instance;
 
+	/**
+	 *  The instance of the configuration.
+	 */
 	private Config config;
+
+	/**
+	 * The instance of the MySQL connection and handler.
+	 */
 	private MySQL sql;
 
+	/**
+	 * The instance of the Score System.
+	 */
 	private ScoreSystem scoreSystem;
+	
+	/**
+	 * The instance of the Game System.
+	 */
 	private GameSystem gameSystem;
+	
+	/**
+	 * The instance of the Statistics System.
+	 */
 	private StatisticsSystem statisticsSystem;
+	
+	/**
+	 * The instance of the Shop System.
+	 */
 	private ShopSystem shopSystem;
 
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
+	 */
 	@Override
 	public void onEnable() {
 		instance = this;
+		
 		config = new Config();
 		config.onEnable();
-		gameSystem = new GameSystem();
-		gameSystem.onEnable();
+		
 		sql = new MySQL(Mineageddon.getInstance(), Mineageddon
 				.getConfigStaticly().getDBHostName(), Mineageddon
 				.getConfigStaticly().getDBPort(), Mineageddon
 				.getConfigStaticly().getDBDatabase(), Mineageddon
 				.getConfigStaticly().getDBUsername(), Mineageddon
 				.getConfigStaticly().getDBPassword());
-		scoreSystem = new ScoreSystem();
-		scoreSystem.onEnable();
-		Gadget.onEnable();
-		this.getCommand("mineageddon").setExecutor(new CommandHandler());
+		
+		gameSystem = new GameSystem();
 		statisticsSystem = new StatisticsSystem();
+		scoreSystem = new ScoreSystem();
+		shopSystem = new ShopSystem();
+		
+		Gadget.onEnable();
+		
+		gameSystem.onEnable();
 		statisticsSystem.onEnable();
+		scoreSystem.onEnable();
 		shopSystem.onEnable();
+		
+		this.getCommand("mineageddon").setExecutor(new CommandHandler());
+		
 	}
 
+	/* (non-Javadoc)
+	 * @see org.bukkit.plugin.java.JavaPlugin#onDisable()
+	 */
 	@Override
 	public void onDisable() {
 		statisticsSystem.onDisable();
@@ -73,43 +115,67 @@ public class Mineageddon extends JavaPlugin {
 		shopSystem = null;
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#config} .
+	 */
 	public Config getConfigInstannce() {
 		return config;
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#instance} .
+	 */
 	public static Mineageddon getInstance() {
 		return instance;
 	}
 
+	/**
+	 * @return Mineageddon's instance of {@link org.bukkit.plugin.java.JavaPlugin#getLogger} .
+	 */
 	public static Logger getLoggerStaticly() {
 		return instance.getLogger();
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#config} .
+	 */
 	public static Config getConfigStaticly() {
 		return instance.getConfigInstannce();
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#scoreSystem} .
+	 */
 	public static ScoreSystem getScoreSystem() {
 		return instance.scoreSystem;
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#sql} .
+	 */
 	public static MySQL getMySQL() {
 		return instance.sql;
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#gameSystem} .
+	 */
 	public static GameSystem getGameSystem() {
 		return instance.gameSystem;
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#statisticsSystem} .
+	 */
 	public static StatisticsSystem getStatisticsSystem() {
 		return instance.statisticsSystem;
 	}
 
+	/**
+	 * @return the instance of {@link com.dexoria.mineageddon.Mineageddon#shopSystem} .
+	 */
 	public ShopSystem getShopSystem() {
 		return shopSystem;
 	}
 
-	public void setShopSystem(ShopSystem shopSystem) {
-		this.shopSystem = shopSystem;
-	}
 }
